7f34289e52014303ef59c49f9ec891c7603bd345
harris.su.malluege@gmail.com <harris.su.malluege@gmail.com>
2020-03-26 13:47:06 -0500

    This commit is to update carousel and jQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+//Implement pause/play functionality to carousel
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
